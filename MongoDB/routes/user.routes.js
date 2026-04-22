44ce02ed2cb19d5a63ef198e179467cb529343b0
Nycwy <stuxy.1129@gmail.com>
2026-04-22 15:22:57 +0545

    Login route created

--- MongoDB/routes/user.routes.js
+++ MongoDB/routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { randomBytes, createHmac } from 'node:crypto';
 import User from '../models/user.model.js';
-
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -32,5 +32,35 @@ router.post('/signup', async (req, res) => {
         data: { id: user._id }
     });
 });
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({
+        email,
+    });
+    if (!existingUser) {
+        return res
+            .status(404)
+            .json({ error: `User with ${email} does not exists` });
+    }
+    const salt = existingUser.salt;
+    const hashed = existingUser.password;
+
+    const newHash = createHmac('sha256', salt).update(password).digest('hex');
+
+    if (hashed !== newHash) {
+        return res.status(400).json({ error: `Invalid Password` });
+    }
+
+    const payload = {
+        _id: existingUser._id,
+        name: existingUser.name,
+        email: existingUser.email
+    }
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+
+    return res.json({ status: "success", token });
+})
 
 export default router;
