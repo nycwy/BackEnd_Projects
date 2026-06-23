ae92620bbe9f73ef13dd05f0f00c2521cb35b1f2
Nycwy <stuxy.1129@gmail.com>
2026-06-23 14:18:18 +0545

    Change Password

--- devsquad/src/routes/profile.routes.js
+++ devsquad/src/routes/profile.routes.js
@@ -3,6 +3,9 @@ const profileRouter = express.Router();
 
 const { userAuth } = require('../middlewares/auth');
 const { validateEditProfile } = require('../utils/signupValidation');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+
 
 // Get Profile
 profileRouter.get('/profile/view', userAuth, async (req, res) => {
@@ -27,6 +30,39 @@ profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
         loggedInUser.set(req.body);
         await loggedInUser.save();
         res.send(`${loggedInUser.firstName}, your profile updated successfully`);
+    } catch (error) {
+        res.status(400).send("ERROR: " + error.message);
+    }
+});
+
+// Change Password
+profileRouter.patch('/profile/password', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const { oldPassword, newPassword } = req.body;
+        if (!oldPassword || !newPassword) {
+            return res.status(400).send("Both old and new passwords are required");
+        }
+        const strongPassword = validator.isStrongPassword(newPassword);
+        if (!strongPassword) {
+            throw new Error("Please enter a strong Password!");
+        }
+        if (oldPassword === newPassword) {
+            return res.status(400).send("New password must be different from the old password");
+        }
+        const userPassword = loggedInUser.password;
+        const isValidPassword = await bcrypt.compare(oldPassword, userPassword);
+
+        if (!isValidPassword) {
+            return res.status(401).send("Incorrect Password");
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        loggedInUser.set({ password: hashedPassword });
+
+        await loggedInUser.save();
+
+        res.status(200).send("Password Changed successfully!");
     } catch (error) {
         res.status(400).send("ERROR: " + error.message);
     }
